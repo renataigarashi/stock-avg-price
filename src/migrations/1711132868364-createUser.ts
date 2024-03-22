48d98f4a7c3dae583fe27d996ba0619a700b898a
Renata Igarashi <rsigarashi@gmail.com>
2024-03-22 15:42:12 -0300

    refactor: migration name

--- src/migrations/1711132868364-createUser.ts
+++ src/migrations/1711132868364-createUser.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Default1711132699914 implements MigrationInterface {
-    name = 'Default1711132699914'
+export class Default1711132868364 implements MigrationInterface {
+    name = 'Default1711132868364'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "tb_users" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_a2c23e0679749c22ffa6c2be910" PRIMARY KEY ("id"))`);
